4b8c81192de9cc3ca4ce921fee93a485bff555d9
Wallis <wallis@pubbly.com>
2020-09-04 21:24:05 -0400

    initial commit

---
+++ SplatoonResult.py
@@ -0,0 +1,90 @@
+import json
+import sys
+import csv
+
+class splatoonResult:
+    def __init__(self, x):
+        with open('weapons.json') as g:
+            y = json.load(g)
+        self.data = x['results']
+        self.weaponData = y
+
+    def __str__(self):
+       return self.data
+
+    def getAllBattleNumbers(self):
+        battleNumbers = []
+        for x in self.data:
+            battleNumbers.append(x['battle_number'])
+
+        return battleNumbers
+
+    def getFirstBattleNumber(self):
+        return int(self.getAllBattleNumbers()[0])
+
+    def getLastBattleNumber(self):
+        return int(self.getAllBattleNumbers()[-1])
+
+    def getSingleMatchInfo(self, battle):
+        row = []
+        row.append(battle['battle_number']) # Unique battle #
+        row.append(battle['start_time']) # Start time + date
+        if "elapsed_time" in battle.keys():      # If turf war, 180 seconds. otherwise store
+            row.append(battle['elapsed_time'])
+        else:
+            row.append(180)
+
+        row.append(battle['rule']['key']) # game rule (splat zones, clam blitz, etc)
+        row.append(battle['my_team_result']['key']) # Win or lose (victory or defeat)
+        row.append(battle['game_mode']['key']) # Game mode (regular, gachi, league) (specifies which kind of league)
+        row.append(battle['type']) # Game type (regular, gachi, league)
+        row.append(battle['stage']['name'].lower() )# stage/map
+        row.append(battle['player_result']['assist_count']) # assisted kills
+        row.append(battle['player_result']['kill_count']) # total kills
+        row.append(battle['player_result']['special_count']) # total specials
+        row.append(battle['player_result']['death_count']) # death_count
+        
+        weapon = (battle['player_result']['player']['weapon']['name'].lower()) # weapon name
+        row.append(weapon)
+
+        row.append(self.weaponData[self.convertWeaponName(weapon)]['type_key']) # weapon type (pulled from JSON)
+        
+        row.append(battle['player_result']['player']['weapon']['sub']['name'].lower()) # sub 
+        row.append(battle['player_result']['player']['weapon']['special']['name'].lower()) # special
+
+        return row
+
+    def generateCSV(self):
+        filename = "master.csv"
+
+        with open(filename, 'r') as f:
+            lines = f.read().splitlines()
+            lastLine = lines[-1]
+
+        lastBattleCaptured = int(lastLine.split(',')[0])
+        
+        rows = []
+
+        for x in range(len(self.data)-1, -1, -1):
+            if int(self.data[x]['battle_number']) > lastBattleCaptured:
+                rows.append(self.getSingleMatchInfo(self.data[x]))  
+                #print(self.data)
+
+        with open(filename, 'a+') as csvfile:    
+            csvwriter = csv.writer(csvfile)  
+            csvwriter.writerows(rows) 
+
+        return len(rows)
+        
+
+    def convertWeaponName(self, name):
+        name = name.replace("-","_")
+        name = name.replace(" ","_")
+        return name.lower()
+
+if __name__ == "__main__":
+    with open('response1.json') as g:
+        y = json.load(g)
+    sp = SplatoonResult(y)
+    #print(sp.getMinMaxBattleNumbers())
+    sp.generateCSV()
---
+++ requestResults.py
@@ -0,0 +1,65 @@
+import requests
+import json
+import random
+import time
+from dotenv import load_dotenv
+import os
+import SplatoonResult
+
+load_dotenv()
+
+def makeRequest():
+    url = "https://app.splatoon2.nintendo.net/api/results"
+
+    payload = {}
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 5.0.1; SCH-I545 Build/LRX22C; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/85.0.4183.81 Mobile Safari/537.36',
+    'cookie': os.getenv("COOKIE")
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    print(response.status_code)
+    return response.json()
+
+def writeResultsToFile(filename, response):
+    with open(filename, 'w') as outfile:
+        json.dump(response, outfile)
+
+if __name__ == "__main__":
+    start = time.time()
+
+    PERIOD_OF_TIME = 61200  # 17 hours
+
+    f = open("counter.txt", "r")
+    counter = int(f.read())
+    f.close()
+
+    while(True):
+        # Wait.....
+        #rnd = random.randint(1620, 3060)
+        rnd = random.randint(3, 8)
+        time.sleep(rnd)
+        print(rnd)
+
+        response = makeRequest()
+        
+        # Add to master.csv
+        sp = SplatoonResult.splatoonResult(response)
+        if(sp.generateCSV()): # it actually DID something, save the file
+            # Increase counter
+            counter += 1
+            # Write counter to file
+            f = open("counter.txt", "w")
+            f.write(str(counter))
+
+            fn = "archive/result" + str(counter) + ".json"
+            # save to archive
+            writeResultsToFile(fn, response)
+
+        # Break at midnight
+        if time.time() > start + PERIOD_OF_TIME : break
+
+        
+
+    
